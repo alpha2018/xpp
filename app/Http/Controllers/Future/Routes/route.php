41573ff25e12f6c75c5718c589489e6ee9293601
QIN <davinciandalien@aliyun.com>
2017-06-11 13:46:56 +0800

    修改app.conf

--- app/Http/Controllers/Future/Routes/route.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Libs\Utils\Sys;
-//Route::group(['middleware'=>'pjax'],function(){
-    
-
-    Route::get('/table', Sys::action('Future.TableController@Index'));
-    Route::get('/form', Sys::action('Future.FormController@Index'));
-//});
-
--- resources/views/backend/search/index.blade.php
+++
@@ -1,7 +0,0 @@
-
-<form class="sui-form form-dark" method="get">
-    <div class="input-control">
-        <input type="text" class="input-medium" name="kw"><i class="sui-icon icon-touch-magnifier"></i>
-        <button type="submit" class="sui-btn btn-primary btn-">Search</button>
-    </div>
-</form>
