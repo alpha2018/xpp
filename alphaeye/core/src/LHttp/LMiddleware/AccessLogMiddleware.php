c79f01347003a91a1428a5ec0824b7c2f0c83a56
QIN <davinciandalien@aliyun.com>
2017-06-25 22:17:15 +0800

    登陆取消 hash清空

---
+++ alphaeye/core/src/LHttp/LMiddleware/AccessLogMiddleware.php
@@ -0,0 +1,25 @@
+<?php namespace AlphaEyeCore\LHttp\LMiddleware;
+use App\Models\AccessLog;
+
+/**
+ * Created by PhpStorm.
+ * User: leo
+ * Date: 2017/6/25
+ * Time: 22:04
+ */
+class AccessLogMiddleware
+{
+    public function handle($request, \Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        //todo
+        $accessLog = app(AccessLog::class);
+        \Log::debug('1', [$request->ip()]);
+        $accessLog->ip = $request->ip();
+        $accessLog->save();
+    }
+}
---
+++ app/Models/AccessLog.php
@@ -0,0 +1,11 @@
+<?php namespace App\Models;
+/**
+ * Created by PhpStorm.
+ * User: leo
+ * Date: 2017/6/25
+ * Time: 22:02
+ */
+class AccessLog extends BaseModel
+{
+    protected $table = 'access_logs';
+}
