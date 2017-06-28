c83df30324cc0dcd1700d0398696b0cde02d1f2e
leo.qin <Leo.Qin@firstleap.cn>
2017-06-28 20:18:21 +0800

    update

---
+++ app/Http/Controllers/Xpp/CollectionController.php
@@ -0,0 +1,22 @@
+<?php namespace App\Http\Controllers\Xpp;
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: leo
+ * Date: 2017/6/28
+ * Time: 下午7:54
+ */
+class CollectionController extends Controller
+{
+    protected $collection;
+
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Xpp/FollowingController.php
@@ -0,0 +1,11 @@
+<?php namespace App\Http\Controllers\Xpp;
+/**
+ * Created by PhpStorm.
+ * User: leo
+ * Date: 2017/6/28
+ * Time: 下午7:56
+ */
+class FollowingController
+{
+
+}
